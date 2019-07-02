92194a9f688c1657a2eeb604e30e022d4bdc74d9
Carlos Eduardo Ferreira <duduferreira_01@hotmail.com>
2019-07-02 15:25:25 -0300

    Arrumando crud

---
+++ README.md
@@ -0,0 +1,1 @@
+# DSW-Projeto3
--- server/grails-app/init/server/BootStrap.groovy
+++ server/grails-app/init/server/BootStrap.groovy
@@ -11,7 +11,10 @@ class BootStrap {
     
         Site globo = new Site (email: 'globo@gmail.com' ,senha: '123', nome: 'Globo', url: 'www.globo.com',telefone: '37444432')
         globo.save()
-
+        Site uol = new Site (email: 'uol@gmail.com' ,senha: '12345', nome: 'Uol', url: 'www.uol.com',telefone: '32313211')
+        uol.save()
+        Teatro arroz = new Site (email: 'arroz@teatro.com' ,senha: '123', cnpj: '111111111111', nome: 'Teatro Arroz',cidade: 'Franca')
+        arroz.save()
 
     }
     def destroy = {
